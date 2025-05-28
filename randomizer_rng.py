dec40fc3bf4b49c2b795cc968b1e26bec05e6e62
dr0pside <111355189+dr0pside@users.noreply.github.com>
2025-05-28 22:14:40 +0300

    Update randomizer_rng.py

--- randomizer_rng.py
+++ randomizer_rng.py
@@ -1275,10 +1275,10 @@ class Randomizer:
                 self.msbio.parts[2].rows[i][PARTSIDX_DATA_COL] = refMsb.parts[2].rows[i][PARTSIDX_DATA_COL]
 
         TARGETNAMES = ['c3230', 'c3300', 'c3350', 'c3370', 'c3410', 'c3530', 'c5210', 'c5360']
-        
-        if self.msbio.parts[2].rows[NAME_DATA_COL][:5] in TARGETNAMES:                #fix broken model changes
-            self.msbio.parts[2].rows[MODEL_DATA_COL] = self.msbio.parts[2].rows[MODEL_DATA_COL] - MODEL_INDEX_DIFF
 
+        if self.msbio.rows[NAME_DATA_COL][:5] in TARGETNAMES:
+            self.msbio.rows[MODEL_DATA_COL] = self.msbio.rows[MODEL_DATA_COL] - MODEL_INDEX_DIFF
+        
         self.msbio.save(self.MAPSTUDIO + "m12_00_00_01.msb")
     def randomize(self, settings, msgArea):
         """
