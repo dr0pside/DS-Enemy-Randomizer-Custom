46ae106c1abf6b6e2b5a7785138500e7c5ef118a
dr0pside <111355189+dr0pside@users.noreply.github.com>
2025-05-28 21:21:27 +0300

    Update randomizer_rng.py

--- randomizer_rng.py
+++ randomizer_rng.py
@@ -1252,12 +1252,25 @@ class Randomizer:
 
         rowCount = len(self.msbio.parts[2].rows)
 
-TARGETNAMES = ['c3230', 'c3300', 'c3350', 'c3370', 'c3410', 'c3530', 'c5210', 'c5360'] #Enemy refs that do not need a model_index_diff
-namedata = self.msbio.parts[2].rows[i][NAME_DATA_COL][:5]
+        TARGETNAMES = ['c3230', 'c3300', 'c3350', 'c3370', 'c3410', 'c3530', 'c5210', 'c5360'] #Enemy refs that do not need a model_index_diff
+        namedata = self.msbio.parts[2].rows[i][NAME_DATA_COL][:5]
 
- for i in range(len(refMsb.parts[2].rows)):
+        for i in range(len(refMsb.parts[2].rows)):
             if (i < rowCount):
-                if (self.isValid(self.msbio.parts[2].rows[i][NAME_DATA_COL][:5])):
+                if namedata in TARGETNAMES:
+                    self.msbio.parts[2].rows[i][MODEL_DATA_COL] = refMsb.parts[2].rows[i][MODEL_DATA_COL]
+                    self.msbio.parts[2].rows[i][NPCAI_DATA_COL] = refMsb.parts[2].rows[i][NPCAI_DATA_COL]
+                    self.msbio.parts[2].rows[i][PARAM_DATA_COL] = refMsb.parts[2].rows[i][PARAM_DATA_COL]
+                    self.msbio.parts[2].rows[i][EVENT_ENTITY_ID_DATA_COL] = refMsb.parts[2].rows[i][EVENT_ENTITY_ID_DATA_COL]
+                    self.msbio.parts[2].rows[i][ANIMID_DATA_COL] = refMsb.parts[2].rows[i][ANIMID_DATA_COL]
+
+                    self.msbio.parts[2].rows[i][POS_DATA_COL] = refMsb.parts[2].rows[i][POS_DATA_COL]
+                    self.msbio.parts[2].rows[i][POS_DATA_COL + 1] = refMsb.parts[2].rows[i][POS_DATA_COL + 1]
+                    self.msbio.parts[2].rows[i][POS_DATA_COL + 2] = refMsb.parts[2].rows[i][POS_DATA_COL + 2]
+                    self.msbio.parts[2].rows[i][POS_DATA_COL + 3] = refMsb.parts[2].rows[i][POS_DATA_COL + 3]
+                    self.msbio.parts[2].rows[i][POS_DATA_COL + 4] = refMsb.parts[2].rows[i][POS_DATA_COL + 4]
+                    self.msbio.parts[2].rows[i][POS_DATA_COL + 5] = refMsb.parts[2].rows[i][POS_DATA_COL + 5]
+                else:
                     self.msbio.parts[2].rows[i][MODEL_DATA_COL] = refMsb.parts[2].rows[i][MODEL_DATA_COL] + MODEL_INDEX_DIFF
                     self.msbio.parts[2].rows[i][NPCAI_DATA_COL] = refMsb.parts[2].rows[i][NPCAI_DATA_COL]
                     self.msbio.parts[2].rows[i][PARAM_DATA_COL] = refMsb.parts[2].rows[i][PARAM_DATA_COL]
@@ -1270,8 +1283,6 @@ namedata = self.msbio.parts[2].rows[i][NAME_DATA_COL][:5]
                     self.msbio.parts[2].rows[i][POS_DATA_COL + 3] = refMsb.parts[2].rows[i][POS_DATA_COL + 3]
                     self.msbio.parts[2].rows[i][POS_DATA_COL + 4] = refMsb.parts[2].rows[i][POS_DATA_COL + 4]
                     self.msbio.parts[2].rows[i][POS_DATA_COL + 5] = refMsb.parts[2].rows[i][POS_DATA_COL + 5]
-            else:
-                self.msbio.AddCreatureRow(refMsb.parts[2].rows[i][:])
 
         for i in range(len(refMsb.parts[2].rows)):
             if (i >= rowCount):
