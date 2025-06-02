a0cb64e966ecaa77e1f146b6ef2d294474792bf3
dr0pside <111355189+dr0pside@users.noreply.github.com>
2025-06-02 15:39:31 +0300

    Update randomizer_rng.py

--- randomizer_rng.py
+++ randomizer_rng.py
@@ -1271,10 +1271,10 @@ class Randomizer:
             if (i >= rowCount):
                 self.msbio.parts[2].rows[i][PARTSIDX_DATA_COL] = refMsb.parts[2].rows[i][PARTSIDX_DATA_COL]
 
-        #EVENTID = {1200801}
-        #for i in range(len(refMsb.parts[2].rows)):
-            #if self.msbio.parts[2].rows[i][EVENT_ENTITY_ID_DATA_COL] in EVENTID:
-                #self.msbio.parts[2].rows[i][MODEL_DATA_COL] = refMsb.parts[2].rows[i][MODEL_DATA_COL]
+        EVENTID = {1200801}     #Moonlight Butterfly model/param fix
+        for i in range(len(refMsb.parts[2].rows)):
+            if self.msbio.parts[2].rows[i][EVENT_ENTITY_ID_DATA_COL] in EVENTID:
+                self.msbio.parts[2].rows[i][MODEL_DATA_COL] = refMsb.parts[2].rows[i][MODEL_DATA_COL]
         
         self.msbio.save(self.MAPSTUDIO + "m12_00_00_01.msb")
     def randomize(self, settings, msgArea):
