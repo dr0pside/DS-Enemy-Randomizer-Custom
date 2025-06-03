9da64d6fb8ab0597b4a294ca837ff5c13309b822
dr0pside <111355189+dr0pside@users.noreply.github.com>
2025-06-04 00:00:41 +0300

    Update randomizer_rng.py

--- randomizer_rng.py
+++ randomizer_rng.py
@@ -82,8 +82,8 @@ class Randomizer:
     # Targets for Easy Asylum
     EASYASYLUM_TARGETS = ['c2232_0000', 'c2500_0000', 'c2500_0001', 'c2500_0002', 'c2500_0003', 'c2500_0005', 'c2500_0006', 'c2500_0007', 'c2500_0009', 'c2500_0010', 'c2500_0011', 'c2550_0000']
 
-    lavabosses = [105, 115, 23, 9, 81]
-    lavaenemies = [23, 81]
+    lavabosses = [105, 115, 23, 9, 81] #taurus, capra, quelaag, centipede + someone I honestly forgot about (not ceaseless)
+    lavaenemies = [23] #only the statue is here for now because enemies not in lavapos (the egg carriers near the bonfire) can end up replaced for some reason as well and I don't want dragon butts
     lavaall = ['c5200', 'c5280', 'c2430', 'c2250', 'c3421', 'c2430', 'c3421']
     lavapos = ['c2250_0003', 'c2250_0004', 'c2250_0005', 'c2250_0006', 'c2250_0007', 'c2250_0008', 'c2250_0009', 'c3421_0000', 'c3421_0001', 'c3421_0002', 'c3421_0003', 'c3421_0004', 'c3421_0005', 'c3421_0006', 'c3421_0007', 'c3421_0008', 'c3421_0009', 'c3421_0010', 'c3421_0011', 'c3421_0012', 'c3421_0013', 'c3421_0014', 'c3421_0015', 'c3421_0016', 'c3421_0017', 'c3421_0018', 'c3421_0019', 'c3421_0020', 'c3421_0021', 'c3421_0022', 'c3421_0023', 'c3421_0024', 'c3421_0025', 'c3421_0026', 'c3421_0027', 'c3421_0028'] #good use of ai
     
@@ -1687,6 +1687,7 @@ class Randomizer:
                                 newAI = self.validNew[newChar][NewCol.AI.value][newAiParamIndex]
                                 newParam = self.validNew[newChar][NewCol.PARAM.value][newAiParamIndex]
                             if str(newAI) == "287001": #Sentinel param fix
+                                newAI == "28700"
                                 newParam = "287010"
                                 
                             paramValue = int(newParam)
